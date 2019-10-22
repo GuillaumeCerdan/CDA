339ef8ccddab5ca339b22e6db3ca50b684a9e8f6
Guillaume Cerdan <gcerdan83@gmail.com>
2019-10-22 11:55:05 +0200

    Comments

--- bot.py
+++ bot.py
@@ -3,14 +3,19 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-
+#Url à parcourir
 requete = requests.get("https://www.getup.agency")
 page = requete.content
+
+# Parser html
 soup = BeautifulSoup(page, "html.parser")
 
+#Tous les liens
 links = soup.find_all("a")
 
-list_links = [links.string.strip() for link in links]
+list_links = [link.string for link in links]
 
+# Les print
+for link in list_links :
+    print(link)
 
-print(list_links)
