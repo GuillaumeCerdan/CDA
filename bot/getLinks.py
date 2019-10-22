865cdec42d51d6c0555e53526ce6c503e1669688
Guillaume Cerdan <gcerdan83@gmail.com>
2019-10-22 15:07:21 +0200

    scraping get data

--- bot/getLinks.py
+++ bot/getLinks.py
@@ -1,20 +1,57 @@
 import os
 import csv
 import requests
+import sys
+import fileinput
 from bs4 import BeautifulSoup
 
-#Url à parcourir
-requete = requests.get("https://www.getup.agency")
-page = requete.content
+# User Agent
+header = {'User-Agent': 'Mozilla/5.0'}
 
-# Parser html
-soup = BeautifulSoup(page, "html.parser")
 
-#Tous les liens
-links = soup.find_all("a")
+fileToSearch = 'departments.csv'
+tempFile = open( fileToSearch, 'r' )
+listdep = []
+listurl = []
+for line in fileinput.input(fileToSearch):
+    listdep.append(line)
+    *_ , ville = line.split(',')
+    ville = ville[:-1].replace(' ','-')
+    listurl.append(ville.strip('"')+".gouv.fr")
+    
 
-list_links = [link.string for link in links]
+listurl.pop(0)
+print (listurl)
 
-# Les print
-for link in list_links :
-    print(link)
+
+for url in listurl:
+
+    goodurl = "http://www." + url
+
+    print("Le site scrappé : " + goodurl)
+
+    #Url à parcourir
+    requete = requests.get(goodurl, headers = header)
+    page = requete.content
+
+    # Parser html
+    soup = BeautifulSoup(page, "html.parser")
+
+    # Tous les liens
+    links = soup.find_all("a")
+    titles = soup.find_all("h1")
+
+    #list_links = [link.string for link in links]
+    list_titles = [title.string for title in titles]
+
+    print(list_titles)
+
+    # Ecriture
+    j = len(list_titles)
+    i = 0
+    with open("donnees.csv", "w", newline='', encoding="utf-8") as fichier:
+        writer = csv.writer(fichier)
+        while i < j:
+            if (list_titles[i] != ""):
+                writer.writerow(str(list_titles[i]))
+            i+=1
--- urlscraping/url-scraping.py
+++ urlscraping/url-scraping.py
@@ -7,25 +7,11 @@ fileToSearch = 'departments.csv'
 tempFile = open( fileToSearch, 'r' )
 listdep = []
 listurl = []
-for line in fileinput.input( fileToSearch):
-    ##print(line)
+for line in fileinput.input(fileToSearch):
     listdep.append(line)
     *_ , ville = line.split(',')
-    ##ville = ville[:-1]
     ville = ville[:-1].replace(' ','-')
-    ##ville = ville.strip('"')
     listurl.append(ville.strip('"')+".gouv.fr")
-    
-    
-
-##listdep.pop(0)
-####print (listdep)
-##for i in listdep:
-##    ##print (i)
-##    *_ , ville = i.split(',')
-##    ville = ville[:-1]
-##    ville = ville.replace(' ','-')
-##    listurl.append(ville+".gouv.fr")
     
 
 listurl.pop(0)
