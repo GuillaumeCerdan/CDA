59790c74fe690222f3ad4483b7621730095b54d1
Guillaume Cerdan <gcerdan83@gmail.com>
2019-11-05 11:07:11 +0100

    tesseract start qui marche pas car Visual Studio pas installé

--- guillaume/bot/scrapping.py
+++ guillaume/bot/scrapping.py
@@ -25,22 +25,4 @@ for url in listurl:
 
     goodurl = "http://www.quevisiter.fr"
 
-    #requete = requests.get(goodurl, headers = header)
-    requete = requests.get(goodurl, allow_redirects=True)
-
-    if requete.history:
-        print("redirigé")
-
-    try:
-        print(requete.content)
-
-    except:
-        print("Chais pas")
-
-    # with open("donneesrobots.csv", "a", newline='', encoding="utf-8") as fichier:
-    #     writer = csv.writer(fichier)
-    #     for i in range(j):
-    #         writer.writerow(list_pre)
-    #         writer.writerow([30 * "_"])
-
     time.sleep(1)
