06f8b00c569aa8bcb2b21b3db4a47ce4965089b5
Guillaume Cerdan <gcerdan83@gmail.com>
2019-11-18 13:59:23 +0100

    Get database POC + Export DB

---
+++ guillaume/database/getData/getValueDb.py
@@ -0,0 +1,31 @@
+import mysql.connector
+
+
+def connectTo(myHost, myUser, myPassword, myDatabase):
+    myconnection = mysql.connector.connect(
+        host = myHost,
+        user = myUser,
+        passwd = myPassword,
+        database = myDatabase
+    )
+    return myconnection
+
+
+def getData(mydb, table):
+    mycursor = mydb.cursor()
+
+    sql = "SELECT * FROM data ORDER BY title"
+
+    mycursor.execute(sql)
+
+    myresult = mycursor.fetchall()
+
+    return myresult
+
+
+mydb = connectTo("localhost", "root", "", "python-upload")
+
+values = getData(mydb, "data")
+
+for v in values:
+    print(v)
