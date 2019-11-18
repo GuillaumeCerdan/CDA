2e142d62b3045700ca4ed4c3aa0f79fba1c8b6ee
Guillaume Cerdan <gcerdan83@gmail.com>
2019-11-18 09:06:26 +0100

    Data + script qui download les pdfs
    Data + script qui download les pdfs

---
+++ guillaume/bot/scrapping_ardeche_poc.py
@@ -0,0 +1,38 @@
+import os
+import csv
+import requests
+import sys
+import fileinput
+from bs4 import BeautifulSoup
+import time
+
+
+# User Agent
+header = {'User-Agent': 'Mozilla/5.0'}
+
+url = "http://www.ardeche.gouv.fr/aout-2019-a8539.html"
+
+
+try:
+    requete = requests.get(url, headers = header)
+    page = requete.content
+except:
+    print("erreur de connection 1")
+
+
+soup = BeautifulSoup(page, "html.parser")
+
+links = soup.find_all("a", href = True)
+list_links = [link.string for link in links]
+i = 0
+for link in links:
+    # Trouve tous les liens pour les recueils
+    if (".pdf" in link.get("href")):
+        text_link = link.text
+        requete.close()
+        pdf_url = 'http://www.ardeche.gouv.fr/' + link.get("href")
+        requete = requests.get(pdf_url, headers = header)
+        page = requete.content
+        open("pdf_ardeche/" + text_link  + ".pdf", 'wb').write(page)
+        i+=1
+        time.sleep(1)
