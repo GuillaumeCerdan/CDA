d6a9edca544b07f344228d9964ff78ae7a5725d4
moneysouci0 <senseythomas99@gmail.com>
2019-12-16 14:07:28 +0100

    ocr qui marche

--- thomas/bot/TestOCR.py
+++ thomas/bot/TestOCR.py
@@ -6,12 +6,12 @@ import os
 
 path = 'D:\\projet\\github\\CDA\\thomas\\bot'
 
-files = []
-# r=root, d=directories, f = files
-for r, d, f in os.walk(path):
-    for file in f:
-        if '.pdf' in file:
-            files.append(os.path.join(r, file))
+# files = []
+# # r=root, d=directories, f = files
+# for r, d, f in os.walk(path):
+#     for file in f:
+#         if '.pdf' in file:
+#             files.append(os.path.join(r, file))
 
 # for f in files:
 #     print(f)
@@ -19,33 +19,33 @@ for r, d, f in os.walk(path):
 # Refacto avec format
 # Rajouter dans le readme les pip
 retourtext = open("test.txt","a")
-# inputpath = r"RAA-test.pdf"
+inputpath = r"RAA-test.pdf"
 pytesseract.pytesseract.tesseract_cmd = r"D:/programme/Tesseract-OCR/tesseract.exe"
 
 outputpath = r"temp/"
 
-# result = pdf2jpg.convert_pdf2jpg(inputpath, outputpath, dpi=300)#on garde les 300 dpi pour avoir une meilleur qualité 
-# # print(str(result))
-# #print("result : " + str(result[0].get("output_jpgfiles")[0]))
-# pytesseract.pytesseract.tesseract_cmd = r"D:/programme/Tesseract-OCR/tesseract.exe"
-# # print(str(result[0].get("output_jpgfiles")[0]))
-# texteImg = pytesseract.image_to_string(Image.open(str(result[0].get("output_jpgfiles")[0])))
+result = pdf2jpg.convert_pdf2jpg(inputpath, outputpath, dpi=300)#on garde les 300 dpi pour avoir une meilleur qualité 
+# print(str(result))
+#print("result : " + str(result[0].get("output_jpgfiles")[0]))
+pytesseract.pytesseract.tesseract_cmd = r"D:/programme/Tesseract-OCR/tesseract.exe"
+# print(str(result[0].get("output_jpgfiles")[0]))
+texteImg = pytesseract.image_to_string(Image.open(str(result[0].get("output_jpgfiles")[0])))
 # print(texteImg)
-# retourtext = open(r"test.txt","a")
-# retourtext.write(texteImg)
-# retourtext.close()
-# # print("coucou")
-# os.startfile("test.txt")
-
-for file in files :
-    inputpath = file
-    result = pdf2jpg.convert_pdf2jpg(inputpath, outputpath, dpi=300)#on garde les 300 dpi pour avoir une meilleur qualité 
-    print(str(result))
-    # print("result : " + str(result[0].get("output_jpgfiles")))
-    # print(str(result[0].get("output_jpgfiles")[0]))
-    texteImg = pytesseract.image_to_string(Image.open(str(result[0].get("output_jpgfiles")[0])))
-    # print(texteImg)
-    retourtext = open(r"test.txt","a")
-    retourtext.write(texteImg)
+retourtext = open(r"test.txt","a")
+retourtext.write(texteImg)
 retourtext.close()
+# print("coucou")
 os.startfile("test.txt")
+
+# for file in files :
+#     inputpath = file
+#     result = pdf2jpg.convert_pdf2jpg(inputpath, outputpath, dpi=300)#on garde les 300 dpi pour avoir une meilleur qualité 
+#     print(str(result))
+#     # print("result : " + str(result[0].get("output_jpgfiles")))
+#     # print(str(result[0].get("output_jpgfiles")[0]))
+#     texteImg = pytesseract.image_to_string(Image.open(str(result[0].get("output_jpgfiles")[0])))
+#     # print(texteImg)
+#     retourtext = open(r"test.txt","a")
+#     retourtext.write(texteImg)
+# retourtext.close()
+# os.startfile("test.txt")
