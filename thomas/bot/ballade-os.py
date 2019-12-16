dec895825e9e688b51ebb5d17137930f1046be2e
moneysouci0 <senseythomas99@gmail.com>
2019-12-16 11:08:17 +0100

    comit 2 pull

--- thomas/bot/ballade-os.py
+++ thomas/bot/ballade-os.py
@@ -6,7 +6,7 @@ files = []
 # r=root, d=directories, f = files
 for r, d, f in os.walk(path):
     for file in f:
-        if '.jpg' in file:
+        if '.pdf' in file:
             files.append(os.path.join(r, file))
 
 for f in files:
